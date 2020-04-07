7b4ce879b21b4dfdb2faef7a6f224fe0e67867a0
nambayasuhiro <yasu18.com@icloud.com>
2020-04-07 11:26:46 +0900

    fix header link and set header sticky

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/', 'PostController@index');
  Route::resource('posts', 'PostController');
 
  if (env('APP_ENV') === 'local') {
