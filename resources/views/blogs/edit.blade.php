ecd07a057a302c107dd856161d9d0d1c1304360e
nambayasuhiro <yasu18.com@icloud.com>
2020-05-11 01:52:55 +0900

    ０５１０

--- resources/views/blogs/edit.blade.php
+++ resources/views/blogs/edit.blade.php
@@ -21,11 +21,11 @@
         <input type="hidden" name="_method" value="PUT">
         <div class="form-group">
         <label for="exampleInputEmail1">Title</label>
-        <input type="text" class="form-control" aria-describedby="emailHelp" name="title" value="{{ old('title') == '' ? $post->title : old('title') }}">
+        <input type="text" class="form-control" aria-describedby="emailHelp" name="title" value="{{ old('title') == '' ? $blog->title : old('title') }}">
     </div>
     <div class="form-group">
         <label for="exampleInputPassword1">Content</label>
-        <textarea class="form-control" name="content">{{ old('content') == '' ? $post->content : old('content') }}</textarea>
+        <textarea class="form-control" name="content">{{ old('content') == '' ? $blog->content : old('content') }}</textarea>
     </div>
     <button type="submit" class="btn btn-outline-primary">Submit</button>
 </form>
--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -19,8 +19,8 @@
                 <p class="card-text">{{ $blog->content }}</p>
 
                 <div class="d-flex" style="height: 36.4px;">
-                <a href="/blogs/{{ $blog->id }}" class="btn btn-outline-primary">Show</a>
-                    <a href="/blogs/{{ $blog->id }}/edit" class="btn btn-outline-primary">Edit</a>
+                <a href="/sample01/blogs/{{ $blog->id }}" class="btn btn-outline-primary">Show</a>
+                    <a href="/sample01/blogs/{{ $blog->id }}/edit" class="btn btn-outline-primary">Edit</a>
                     <form action="/blogs/{{ $blog->id }}" method="POST" onsubmit="if(confirm('Delete? Are you sure?')) { return true } else {return false };">
                         <input type="hidden" name="_method" value="DELETE">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
@@ -36,3 +36,4 @@
 @endsection
 
 
+　
